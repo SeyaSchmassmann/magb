f9b48a5292fa358f7752f717bc62f3632254b482
seya.schmassmann <seya.schmassmann@students.fhnw.ch>
2024-02-26 00:46:25 +0100

    wip dithering working version

---
+++ src/imageprocessing/colors/DitheringConverter.java
@@ -0,0 +1,236 @@
+package imageprocessing.colors;
+
+import org.eclipse.swt.graphics.ImageData;
+
+import imageprocessing.IImageProcessor;
+import imageprocessing.ImageProcessing;
+
+public class DitheringConverter implements IImageProcessor {
+
+    private static final int THRESHOLD = 127;
+
+    @Override
+    public boolean isEnabled(int imageType) {
+        return true;
+    }
+
+    @Override
+    public ImageData run(ImageData inData, int imageType) {
+        return dither(inData, imageType);
+    }
+
+
+    public static ImageData dither(ImageData inData, int imageType) {
+        var outputImage = GrayScaleConverter.convert(inData, imageType);
+
+        int[][] ditherMatrix = {
+                {0, 0, 0, 8, 4},
+                {2, 4, 8, 4, 2},
+                {1, 2, 4, 2, 1}
+        };
+
+        for (int y = 0; y < inData.height; y++) {
+            for (int x = 0; x < inData.width; x++) {
+                int oldGray = 0xFF & outputImage.data[y * outputImage.bytesPerLine + x];
+                
+                int newGray = oldGray < 128 ? 0 : 255;
+                outputImage.data[y * outputImage.bytesPerLine + x] = (byte)newGray;
+
+                int error = oldGray - newGray;
+                distributeError(outputImage, x, y, inData.width, inData.height, error, ditherMatrix);
+            }
+        }
+
+        return outputImage;
+    }
+
+    private static void distributeError(ImageData inData, int x, int y, int width, int height, int error, int[][] matrix) {
+        for (int yOffset = 0; yOffset <= 2; yOffset++) {
+            for (int xOffset = -2; xOffset <= 2; xOffset++) {
+                int neighborX = x + xOffset;
+                int neighborY = y + yOffset;
+
+                if (neighborX >= 0 && neighborX < width && neighborY >= 0 && neighborY < height) {
+                    int gray = 0xFF & inData.data[neighborY * inData.bytesPerLine + neighborX];
+                    var newGray = gray + error * (double)(matrix[yOffset][xOffset + 2]) / 42;
+
+                    if (newGray < 0) {
+                        newGray = 0;
+                    } else if (newGray > 255) {
+                        newGray = 255;
+                    }
+
+                    int gray1 = ImageProcessing.clamp8(newGray);
+                    inData.data[neighborY * inData.bytesPerLine + neighborX] = (byte)gray1;
+                    // inData.setPixel(neighborX, neighborY, gray1);
+                }
+            }
+        }
+    }
+
+    private static void distributeError2(ImageData inData, int x, int y, int width, int height, int error, int[][] matrix) {
+        for (int yOffset = 0; yOffset <= 2; yOffset++) {
+            for (int xOffset = -2; xOffset <= 2; xOffset++) {
+                int neighborX = x + xOffset;
+                int neighborY = y + yOffset;
+
+                if (neighborX >= 0 && neighborX < width && neighborY >= 0 && neighborY < height) {
+                    var gray = inData.getPixel(neighborX, neighborY);
+                    var newGray = gray + error * (double)(matrix[yOffset][xOffset + 2]) / 42;
+
+                    if (newGray < 0) {
+                        newGray = 0;
+                    } else if (newGray > 255) {
+                        newGray = 255;
+                    }
+
+                    int gray1 = ImageProcessing.clamp8(newGray);
+                    inData.setPixel(neighborX, neighborY, gray1);
+                }
+            }
+        }
+    }
+
+
+    public static ImageData dither1(ImageData inData, int imageType) {
+        var matrix = new double[] {
+                8.0 / 42, 4.0 / 42, 2.0 / 42,
+                4.0 / 42, 8.0 / 42, 4.0 / 42,
+                2.0 / 42, 4.0 / 42, 2.0 / 42
+        };
+        var matrixDimension = 3;
+        var matrixStartIndex = matrixDimension / 2;
+    
+        var grayScaleImage = GrayScaleConverter.convert(inData, imageType);
+    
+        for (int y = 0; y < grayScaleImage.height; y++) {
+            for (int x = 0; x < grayScaleImage.width; x++) {
+    
+                var pixelValue = grayScaleImage.getPixel(x, y);
+                if (pixelValue < 0) {
+                    pixelValue = 0;
+                } else if (pixelValue > 255) {
+                    pixelValue = 255;
+                }
+    
+                var newPixel = pixelValue < 128 ? 0 : 255;
+                grayScaleImage.setPixel(x, y, newPixel);
+                var error = pixelValue - newPixel;
+    
+                for (int row = -matrixStartIndex; row <= matrixStartIndex; row++) {
+                    for (int col = -matrixStartIndex; col <= matrixStartIndex; col++) {
+                        int ix = x + col;
+                        int iy = y + row;
+    
+                        if (ix < 0 || ix >= grayScaleImage.width || iy < 0 || iy >= grayScaleImage.height) {
+                            continue;
+                        }
+    
+                        int matrixIndex = (row + matrixStartIndex) * matrixDimension + (col + matrixStartIndex);
+                        double val = matrix[matrixIndex];
+                        int gray = ImageProcessing.clamp8((int) (error * val) + grayScaleImage.getPixel(ix, iy));
+                        grayScaleImage.setPixel(ix, iy, gray);
+                    }
+                }
+            }
+        }
+    
+        return grayScaleImage;
+    }
+
+
+    public static ImageData dither2(ImageData inData, int imageType) {
+        var matrix = new double[] {
+            8 / 42, 4 / 42,
+            2 / 42, 4 / 42, 8 / 42, 4 / 42, 2 / 42,
+            1 / 42, 2 / 42, 4 / 42, 2 / 42, 1 / 42
+        };
+        var matrixDimension = 3;
+        var matrixStartIndex = matrixDimension / 2;
+    
+        var grayScaleImage = GrayScaleConverter.convert(inData, imageType);
+    
+        for (int y = 0; y < grayScaleImage.height; y++) {
+            for (int x = 0; x < grayScaleImage.width; x++) {
+    
+                var pixelValue = grayScaleImage.getPixel(x, y);
+                if (pixelValue < 0) {
+                    pixelValue = 0;
+                } else if (pixelValue > 255) {
+                    pixelValue = 255;
+                }
+    
+                var newPixel = pixelValue < 128 ? 0 : 255;
+                grayScaleImage.setPixel(x, y, newPixel);
+                var error = pixelValue - newPixel;
+    
+                for (int row = -matrixStartIndex; row <= matrixStartIndex; row++) {
+                    for (int col = -matrixStartIndex; col <= matrixStartIndex; col++) {
+                        int ix = x + col;
+                        int iy = y + row;
+    
+                        if (ix < 0 || ix >= grayScaleImage.width || iy < 0 || iy >= grayScaleImage.height) {
+                            continue;
+                        }
+    
+                        int matrixIndex = (row + matrixStartIndex) * matrixDimension + (col + matrixStartIndex);
+                        double val = matrix[matrixIndex];
+                        int gray = ImageProcessing.clamp8((int) (error * val) + grayScaleImage.getPixel(ix, iy));
+                        grayScaleImage.setPixel(ix, iy, gray);
+                    }
+                }
+            }
+        }
+    
+        return grayScaleImage;
+    }
+
+
+    public static ImageData ditherSelf(ImageData inData, int imageType) {
+        var matrix = new double[] {
+            8 / 42, 4 / 42,
+            2 / 42, 4 / 42, 8 / 42, 4 / 42, 2 / 42,
+            1 / 42, 2 / 42, 4 / 42, 2 / 42, 1 / 42
+        };
+        var matrixStartIndex = matrix.length;
+        var matrixDimension = 5;
+
+        var grayScaleImage = GrayScaleConverter.convert(inData, imageType);
+
+        for (int x = 0; x < grayScaleImage.width; x++) {
+            for (int y = 0; y < grayScaleImage.height; y++) {
+
+                var pixelValue = grayScaleImage.getPixel(x, y);
+                if (pixelValue < 0) {
+                    pixelValue = 0;
+                } else if (pixelValue > 255) {
+                    pixelValue = 255;
+                }
+
+                var newPixel = pixelValue < THRESHOLD ? 0 : 255;
+                grayScaleImage.setPixel(x, y, newPixel);
+                var error = pixelValue - newPixel;
+       
+                for (var i = 0; i < matrix.length; i++) {
+                    var val = matrix[i];
+                    var index = matrixStartIndex + i;
+                    var col = (index / matrixDimension) - 2;
+                    var row = (index % matrixDimension) - 2;
+                    
+                    var ix = x + col;
+                    var iy = y + row;
+       
+                    if (ix < 0 || ix >= grayScaleImage.width || iy < 0 || iy >= grayScaleImage.height) {
+                        continue;
+                    }
+
+                    int gray = ImageProcessing.clamp8((error * val) + grayScaleImage.getPixel(ix, iy));
+                    grayScaleImage.setPixel(ix, iy, gray);
+                }
+            }
+        }
+
+        return grayScaleImage;
+    }
+    
+}
--- src/imageprocessing/colors/GrayScaleConverter.java
+++ src/imageprocessing/colors/GrayScaleConverter.java
@@ -1,21 +1,41 @@
 package imageprocessing.colors;
 
 import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.graphics.RGB;
 
 import imageprocessing.IImageProcessor;
+import imageprocessing.ImageProcessing;
+import main.Picsi;
+import utils.Parallel;
 
 public class GrayScaleConverter implements IImageProcessor {
 
 	@Override
 	public boolean isEnabled(int imageType) {
-		return true;
+        return imageType != Picsi.IMAGE_TYPE_GRAY && imageType != Picsi.IMAGE_TYPE_INDEXED;
 	}
 
 	@Override
 	public ImageData run(ImageData inData, int imageType) {
-		ImageData outData = (ImageData)inData.clone();
-        
-		return outData;
+        return convert(inData, imageType);
 	}
-    
+ 
+	/**
+	 * Invert image data
+	 * @param output image
+	 * @param imageType
+	 */
+	public static ImageData convert(ImageData inData, int imageType) {
+        var grayScaleImage = ImageProcessing.createImage(inData.width, inData.height, Picsi.IMAGE_TYPE_GRAY);
+
+        Parallel.For(0, inData.height, y -> {
+            for (int x = 0; x < inData.width; x++) {
+                var rgb = inData.palette.getRGB(inData.getPixel(x, y));
+                int gray = ImageProcessing.clamp8(0.299 * rgb.red + 0.587 * rgb.green + 0.114 * rgb.blue);
+                grayScaleImage.setPixel(x, y, gray);
+            }
+        });
+
+        return grayScaleImage;
+    }
 }
